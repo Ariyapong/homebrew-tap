d93b1cc83207f39f7f911a30680e77050db9415f
Ariyapong Wimolnoch <ariyapong.wim@axonstech.com>
2026-06-18 00:09:35 +0700

    chore(formula): keybook 0.6.0

--- Formula/keybook.rb
+++ Formula/keybook.rb
@@ -1,8 +1,8 @@
 class Keybook < Formula
   desc "macOS TUI for searching keyboard shortcuts and recipes"
   homepage "https://github.com/Ariyapong/keybook"
-  url "https://registry.npmjs.org/@arthony/keybook/-/keybook-0.5.0.tgz"
-  sha256 "7a795781cde7fff28c848c5bf8d9426e21169e16c85db863daa0dbdd507b733f"
+  url "https://registry.npmjs.org/@arthony/keybook/-/keybook-0.6.0.tgz"
+  sha256 "3bb45125bec786a69e41d9c4cc772d7e4a902f6251db87e90ed751d05acfd816"
   license "MIT"
 
   depends_on "node@22"
